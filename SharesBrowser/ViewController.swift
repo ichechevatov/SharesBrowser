c8c5da5d7a75259e21436adfd3b699fd52f79093
Ïlya Chechevatov <uo.uo@list.ru>
2021-04-07 16:04:52 +0400

    Initial Commit

---
+++ SharesBrowser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SharesBrowser
+//
+//  Created by Илья Че on 07.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
